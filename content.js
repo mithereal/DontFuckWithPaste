a71da62965c2b4f4c30cb13c3bea54c39df281d6
Jason Clark <mithereal@gmail.com>
2018-11-02 16:16:53 -0700

    Unblock copy and paste on more sites, and unblock cut in general

--- content.js
+++ content.js
@@ -1,18 +1,44 @@
-const forceBrowserDefault = function(e){
-  e.stopImmediatePropagation();
-  return true;
+const allowDefaultAction = function(e){
+  // prevent custom event handlers on the page swallowing the event
+  e.stopImmediatePropagation();	  e.stopImmediatePropagation();
+  return true;	  return true;
+};	};
+ const allowDefaultCtrlAction = function(e){
+  if (e.type !== "keydown") return false;
+   const CTRL_KEY_CODE = 17;
+  if (parseInt(e.keyCode) !== CTRL_KEY_CODE) return false;
+   return allowDefaultAction(e);
 };
+ const allowDefaultRightClickAction = function(e){
+  if (e.type !== "mousedown") return false;
+   const RIGHT_CLICK = 2;
+  if (e.button !== RIGHT_CLICK) return false;
+   return allowDefaultAction(e);
+};
+ chrome.storage.sync.get(window.defaultValues, function({exclude, include}) {	chrome.storage.sync.get(window.defaultValues, function({exclude, include}) {
+  const excludes = new RegExp(exclude.split('\n').join('|'));	  const excludes = new RegExp(exclude.split('\n').join('|'));
+  const includes = new RegExp(include.split('\n').join('|'));	  const includes = new RegExp(include.split('\n').join('|'));
+  const location = window.location.href;	  const location = window.location.href;
+   if (includes.test(location) && !excludes.test(location)) {	  if (includes.test(location) && !excludes.test(location)) {
+    document.addEventListener('copy', allowCopyAndPaste, true);	    document.addEventListener('cut', allowDefaultAction, true);
+    document.addEventListener('paste', allowCopyAndPaste, true);	    document.addEventListener('copy', allowDefaultAction, true);
+    document.addEventListener('paste', allowDefaultAction, true);
+    document.addEventListener('keydown', allowDefaultCtrlAction, true);
+    document.addEventListener('mousedown', allowDefaultRightClickAction, true);
+    document.addEventListener('contextmenu', allowDefaultAction, true);
+    document.addEventListener('selectstart', allowDefaultAction, true);
+  }	  }
+});	});
 
-chrome.runtime.onMessage.addListener(({ active }) => {
-  if (active) {
-    document.addEventListener('copy', forceBrowserDefault, true);
-    document.addEventListener('cut', forceBrowserDefault, true);
-    document.addEventListener('paste', forceBrowserDefault, true);
-  } else {
-    document.removeEventListener('copy', forceBrowserDefault, true);
-    document.removeEventListener('cut', forceBrowserDefault, true);
-    document.removeEventListener('paste', forceBrowserDefault, true);
-  }
-});
-
-chrome.runtime.sendMessage({ didLoad: true });
+10  test/iframe.html
+@@ -1,8 +1,10 @@
+<script src="script.js"></script>	<script src="script.js"></script>
+<p>(This is a form inside of an iframe.)</p>	<p onselectstart="return false;" >(This is a form inside of an iframe.)</p>
+<form>	<form onselectstart="return false;" >
+  <label for="unpastable">Go ahead, try to copy or paste</label>	  <label>Go ahead, try to copy or paste:</label>
+  <input id="attribute" type="text" oncopy="return false;" onpaste="return false;" />	  <input id="attribute" type="text" oncopy="return false;" onpaste="return false;" oncut="return false;"
+         onkeydown="return alertOnCtrl(event);"
+         onmousedown="return alertOnRightClick(event);" oncontextmenu="return false;"/>
+  <input id="property" type="text" />	  <input id="property" type="text" />
+  <input id="listener" type="text" />	  <input id="listener" type="text" />
+</form>	</form>
--- test/script.js
+++ test/script.js
@@ -1,13 +1,31 @@
-window.onload = function(){
-  const preventDefault = e => e.preventDefault();
-
-  const listener = document.getElementById('listener');
-  const property = document.getElementById('property');
-
-  listener.addEventListener('copy', preventDefault, false);
-  listener.addEventListener('cut', preventDefault, false);
-  listener.addEventListener('paste', preventDefault, false);
-  property.oncopy = preventDefault;
-  property.oncut = preventDefault;
-  property.onpaste = preventDefault;
+function alertOnCtrl(e) {
+    const CTRL_KEY_CODE = 17;
+    if (parseInt(e.keyCode) === CTRL_KEY_CODE) {
+        // popping up an alert box is enough to stop default event handling in chrome!
+        alert("ctrl shortcuts are blocked");
+    }
+    return true;
+}
+function alertOnRightClick(e) {
+    const RIGHT_CLICK = 2;
+    if (e.button === RIGHT_CLICK) {
+        alert("right click menu is blocked");
+    }
+    return true;
+}
+window.onload = function(){	window.onload = function(){
+    var preventCopyAndPaste = function(e){ e.preventDefault(); }	  const preventEvent = function(e){ e.preventDefault(); };
+    document.getElementById('property').onpaste = preventCopyAndPaste;
+    document.getElementById('property').oncopy = preventCopyAndPaste;	  document.getElementById('property').onpaste = preventEvent;
+    document.getElementById('listener').addEventListener('copy', preventCopyAndPaste, false);	  document.getElementById('property').oncopy = preventEvent;
+    document.getElementById('listener').addEventListener('paste', preventCopyAndPaste, false);	  document.getElementById('property').oncut = preventEvent;
+};	  document.getElementById('property').onkeydown = alertOnCtrl;
+    document.getElementById('property').onmousedown = alertOnRightClick;
+    document.getElementById('property').oncontextmenu = preventEvent;
+    document.getElementById('listener').addEventListener('copy', preventEvent, false);
+    document.getElementById('listener').addEventListener('paste', preventEvent, false);
+    document.getElementById('listener').addEventListener('cut', preventEvent, false);
+    document.getElementById('listener').addEventListener('keydown', alertOnCtrl, false);
+    document.getElementById('listener').addEventListener('mousedown', alertOnRightClick, false);
+    document.getElementById('listener').addEventListener('contextmenu', preventEvent, false);
 };
